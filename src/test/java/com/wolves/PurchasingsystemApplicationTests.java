28c7058c230d71f41b8282e38bf5c4562c7dd33b
ar <2947877197@qq.com>
2021-11-01 15:23:57 +0800

    Initial commit

---
+++ src/test/java/com/wolves/PurchasingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
